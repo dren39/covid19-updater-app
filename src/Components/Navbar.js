6f7dc82a80bd92225ea42c6748bfa3957766e3dc
David Ren <dren391@gmail.com>
2020-06-01 18:43:58 -0400

    Navbar setup with grid, styling started

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import {Link, withRouter} from 'react-router-dom';
+import '../Styles/Navbar.css';
 
 function Navbar() {
     return (
         <div className="top-bar-wrapper">
-            <div className="menu-bar-wrapper">
+            <div class="title-wrapper">
+                <h3>Covid19 Daily News</h3>
+            </div>
+            
+            <div class="menu-bar-wrapper">
+                <ul class="menu-bar">
+                    <li class="home-nav">
+                        <Link to={"/"}>
+                            <p>Home</p>
+                        </Link>
+                    </li>
+                    <li class="about-nav">
+                        <Link to={"/about"}>
+                            <p>About</p>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            {/* <div className="menu-bar-wrapper">
                 <div className="title-wrapper">
                     <h3>Covid19 Daily News</h3>
                 </div>
@@ -20,7 +39,7 @@ function Navbar() {
                         </Link>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
