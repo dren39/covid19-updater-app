19fe46afdc30f246628289f9d23145cbe5a57bc8
David Ren <dren391@gmail.com>
2020-06-13 18:52:10 -0400

    About page written, need to finish styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../Styles/About.css'
 
 function About() {
     return (
-        <div>
-            <p>This is about page</p>
+        <div className="about-wrapper">
+            <p>
+                Covid19 Daily Update is a web application developed to help spread information
+                and awareness about the 2019 coronavirus global pandemic. The app's main functions
+                is to display statistics for infection cases within a given state, and 
+                toggle to a news page where users will find the latest news article
+                related to Covid19 within that State.
+            </p>
+            <p>
+                This app is powered by the 
+                <span>
+                    <a href="https://developer.smartable.ai/api-details#api=coronavirus"> Smartable AI API</a>
+                </span>
+            </p>
+            <a href="https://github.com/dren39/covid19-updater-app">Github Repository</a>
         </div>
     )
 }
