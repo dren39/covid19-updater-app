87c16e1866278eea10b9ad215cc265fff1b6d553
David Ren <dren391@gmail.com>
2020-06-22 23:50:38 -0400

    Updates

---
+++ precache-manifest.7c2025d29880943e363e2c171ec45ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab16d676925e5fb295adee6755d12fc",
+    "url": "/covid19-updater-app/index.html"
+  },
+  {
+    "revision": "54e55a624dfbb1e8b8db",
+    "url": "/covid19-updater-app/static/css/main.99f3cad5.chunk.css"
+  },
+  {
+    "revision": "75d16f0942c70c5b60f3",
+    "url": "/covid19-updater-app/static/js/2.13eccfb2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/covid19-updater-app/static/js/2.13eccfb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e55a624dfbb1e8b8db",
+    "url": "/covid19-updater-app/static/js/main.0efc0c89.chunk.js"
+  },
+  {
+    "revision": "d6ffb34f0a39401739bd",
+    "url": "/covid19-updater-app/static/js/runtime-main.65302f8c.js"
+  }
+]);
