1948c39b88fc613b1b3240c04f984115f93d9458
David Ren <dren391@gmail.com>
2020-06-23 09:48:01 -0400

    change base name

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter basename={"/"}>
+            <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
                 <div className="App">
                     <Navbar/>
                     <Switch>
